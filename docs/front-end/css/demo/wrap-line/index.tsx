a3b86064233cb102c9673c0473517e2ffe9da224
LEMON <2102028233@qq.com>
2023-10-07 15:57:09 +0800

    draft: css 换行

---
+++ docs/front-end/css/demo/wrap-line/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+const WrapLine = () => {
+  return (
+    <div className={styles['hide-scrollbar']}>
+      <div>青青园中葵，朝露待日晞。</div>
+      <div>阳春布德泽，万物生光辉。</div>
+      <div>常恐秋节至，焜黄华叶衰。</div>
+      <div>百川东到海，何时复西归？</div>
+      <div>少壮不努力，老大徒伤悲！</div>
+    </div>
+  );
+};
+
+export default WrapLine;
