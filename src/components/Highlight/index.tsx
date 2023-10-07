6f1e84b2ed1f0809166a0ef54b0a58a1f3c6102d
LEMON <2102028233@qq.com>
2023-10-07 15:54:54 +0800

    feat(components): 新增更多色彩支持

--- src/components/Highlight/index.tsx
+++ src/components/Highlight/index.tsx
@@ -6,14 +6,22 @@ enum HighlightColor {
   green = 'green',
   red = 'red',
   yellow = 'yellow',
+  gray = 'gray',
+  'sky-blue' = 'sky-blue',
 }
 
 type IProps = React.PropsWithChildren<{
   color?: HighlightColor;
+  className?: string;
+  style?: React.CSSProperties;
 }>;
 
-const Highlight: React.FC<IProps> = ({ children, color = 'green' }) => {
-  return <span className={clsx(styles[`highlight`], styles[color])}>{children}</span>;
+const Highlight: React.FC<IProps> = ({ children, color = 'green', className, style }) => {
+  return (
+    <span className={clsx(styles[`highlight`], styles[color], className)} style={style}>
+      {children}
+    </span>
+  );
 };
 
 export default Highlight;
